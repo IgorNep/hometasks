441e2cb2b1c2238d813fb8011aac5b55a1fc18e8
Igor <igornesan@gmail.com>
2019-07-10 20:59:57 +0300

    aplle site two

--- build/apple_site_sample.js
+++ build/apple_site_sample.js
@@ -91,9 +91,9 @@
   !*** ./src/apple_site_sample/apple_site_sample.css ***!
   \*****************************************************/
 /*! no static exports found */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
-throw new Error("Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/lib/loader.js):\n\r\nwidth: 100%:;\r\n      ^\r\n      Invalid CSS after \"width: 100%\": expected \"}\", was \":;\"\r\n      in C:\\Users\\admin 1\\Desktop\\HomeTasks\\group_2206\\src\\apple_site_sample\\apple_site_sample.css (line 52, column 8)\n    at runLoaders (C:\\Users\\admin 1\\Desktop\\HomeTasks\\group_2206\\node_modules\\webpack\\lib\\NormalModule.js:301:20)\n    at C:\\Users\\admin 1\\Desktop\\HomeTasks\\group_2206\\node_modules\\loader-runner\\lib\\LoaderRunner.js:367:11\n    at C:\\Users\\admin 1\\Desktop\\HomeTasks\\group_2206\\node_modules\\loader-runner\\lib\\LoaderRunner.js:233:18\n    at context.callback (C:\\Users\\admin 1\\Desktop\\HomeTasks\\group_2206\\node_modules\\loader-runner\\lib\\LoaderRunner.js:111:13)\n    at Object.asyncSassJobQueue.push [as callback] (C:\\Users\\admin 1\\Desktop\\HomeTasks\\group_2206\\node_modules\\sass-loader\\lib\\loader.js:55:13)\n    at Object.done [as callback] (C:\\Users\\admin 1\\Desktop\\HomeTasks\\group_2206\\node_modules\\neo-async\\async.js:8077:18)\n    at options.error (C:\\Users\\admin 1\\Desktop\\HomeTasks\\group_2206\\node_modules\\node-sass\\lib\\index.js:294:32)");
+// extracted by mini-css-extract-plugin
 
 /***/ }),
 
